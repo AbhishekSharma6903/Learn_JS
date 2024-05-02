2d4853730437d9a6fe96e2910e31a6477bc0cf66
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-02 18:32:29 +0000

    worked on dataypes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,1 +1,10 @@
-console.log();
+const accId = 123123
+let accEmail = "abhi@gmail.com"
+var accPass = "123"
+accCity = "chennai"
+let accState
+
+console.table([accId, accEmail, accPass, accCity, accState])
+
+// accId = 3333 -> cannot change const
+// avoid using var in JS -> problem of scoping
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS version as newer Version 
+
+let name = "abhi"
+let age = 18
+let good = true
+let state;
+
+
+/*
+DATA_TYPES:
+-- number => 2^53 (approx limit)
+-- bigint
+-- string => ""
+-- bool => true/false
+-- null => standalone value
+-- underdefined =>
+-- symbol => unique
+ */
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);   //object => IMPORTANT FOR INTERVIEW
