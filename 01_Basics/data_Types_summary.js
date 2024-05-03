b32c4895a0ba14c89f56b412e8dc4371a8182a73
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-03 11:40:11 +0000

    types of dataTypes

---
+++ 01_Basics/data_Types_summary.js
@@ -0,0 +1,19 @@
+//Primitive
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+//Reference(Non-primitive)
+// Array, Objects, Functions
+const alphas = ['a', 'b', 'c']
+
+let myobj = {
+    name : 'Abhi',
+    age: 22
+}
+
+const myfun = function(){
+    console.log('hello')
+}
+
+console.log(typeof alphas);
+console.log(typeof myobj);
+console.log(typeof myfun);
