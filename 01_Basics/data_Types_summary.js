8ae6495f1e8ab74ed61ec8d7a64ed5b9f7bb8562
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-03 12:03:24 +0000

    memory and reference in dataTypes

--- 01_Basics/data_Types_summary.js
+++ 01_Basics/data_Types_summary.js
@@ -1,7 +1,11 @@
-//Primitive
+//Primitive  => "STACK" , always copy is passed as reference
+
 // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
-//Reference(Non-primitive)
+
+
+//Reference(Non-primitive)   => "HEAP" , always original is passed as reference
+
 // Array, Objects, Functions
 const alphas = ['a', 'b', 'c']
 
@@ -9,6 +13,7 @@ let myobj = {
     name : 'Abhi',
     age: 22
 }
+console.log(myobj.name) // accessing an object 
 
 const myfun = function(){
     console.log('hello')
