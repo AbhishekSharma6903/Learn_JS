fc3c28ded3c77a255c73110fd9123756e464ea04
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-03 06:10:15 +0000

    Learning conversions

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,19 @@
+let score = "Abhishek"
+
+console.log(typeof score);
+
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber);
+console.log(scoreInNumber);
+
+//Converstion to Numbers
+// "33" => 33
+// "33abc" => NaN 
+// true => 1 && false => 0
+
+/*
+Similary for Boolean
+1 => true && 0 => False
+"" => False
+"Abhishek" => true
+*/
