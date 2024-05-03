3a5599817435c4f8a22ebacf03cc2d8e06238a99
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-03 07:05:16 +0000

    worked on conversions

---
+++ 01_Basics/04_conversion.js
@@ -0,0 +1,11 @@
+// console.log("2" > 3);
+// console.log("2" == 2);
+// console.log("2" === 2); // strict-check datatype as well as operation
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // for >= null is treated as 0
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
