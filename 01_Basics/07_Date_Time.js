a1caed0f253c4a395a4779c7a1ec3a55ee611312
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-04 16:54:33 +0000

    Dates and time

---
+++ 01_Basics/07_Date_Time.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let mycreation = new Date(2023, 0, 23)
+// let mycreation = new Date(2023, 0, 23,5,3)
+// let mycreation = new Date('2023-01-23')
+let mycreation = new Date('01-23-2023')
+console.log(mycreation.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(mycreation.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
