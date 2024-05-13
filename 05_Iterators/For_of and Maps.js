dabdcfa7f1b6eb92e7e58bdfb941ff3863c18036
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-13 12:06:42 +0000

    Updated maps and For of

---
+++ 05_Iterators/For_of and Maps.js
@@ -0,0 +1,38 @@
+// For of
+//["","",""]
+//[{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+for(const num of arr){
+    // console.log(num);
+}
+
+const gretings = "Hello Solitude"
+for(const greet of gretings){
+    // console.log(`Each char is ${greet}`);
+}
+
+
+//MAPS
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// } 
+//CANNOT ACCESS OBJECTS WITH { For of }
