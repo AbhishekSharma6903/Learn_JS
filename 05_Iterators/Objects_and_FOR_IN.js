b71d74ef3f4a61ac7a696cbf9199e8e8604cec55
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-13 12:29:35 +0000

    FOR in

---
+++ 05_Iterators/Objects_and_FOR_IN.js
@@ -0,0 +1,28 @@
+const myObject ={
+    js: "javaScript",
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for(const key in myObject){
+    // console.log(`${key} shortcut if for ${myObject[key]}`);  
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in programming){
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+//MAPS are not iteratable with {FOR IN}
