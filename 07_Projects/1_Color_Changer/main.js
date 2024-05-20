5bfab2be5d9bdd5a2c96b7601ba272c5ff7c71f3
ABHISHEK SHARMA <99632495+AbhishekSharma6903@users.noreply.github.com>
2024-05-20 15:32:55 +0000

    project 1_color_changer

---
+++ 07_Projects/1_Color_Changer/main.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (action) {
+    console.log(action);
+    console.log(action.target);
+    if (action.target.id === 'grey') {
+      body.style.backgroundColor = action.target.id;
+    }
+    if (action.target.id === 'white') {
+      body.style.backgroundColor = action.target.id;
+    }
+    if (action.target.id === 'blue') {
+      body.style.backgroundColor = action.target.id;
+    }
+    if (action.target.id === 'yellow') {
+      body.style.backgroundColor = action.target.id;
+    }
+  });
+});
